8ffc8e20aa128fd18fcd2e1ef03e8f9405168d3e
Gabriel Lopes <carnagesagas@gmail.com>
2024-04-29 10:25:31 -0300

    adicionado um header

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from tkinter import*
 
+from PIL import Image, ImageTk
+
+
 co0 = "#f0f3f5"  # Preta
 co1 = "#feffff"  # branca
 co2 = "#3fb5a3"  # verde
@@ -13,10 +16,22 @@ janela.geometry('350x200')
 janela.configure(background=co1)
 janela.resizable(width=FALSE, height=FALSE)
 
-frame_logo = Frame(janela, width=350, height=60, bg=co2)
+frame_logo = Frame(janela, width=350, height=60, bg=co1)
 frame_logo.grid(row=0, column=0, pady=1, padx=0, sticky=NSEW)
 
-frame_corpo = Frame(janela, width=350, height=140, bg=co2)
+frame_corpo = Frame(janela, width=350, height=140, bg=co1)
 frame_corpo.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+
+imagem = Image.open('speed.png')
+imagem_resized = imagem.resize((55, 60))
+imagem_tk = ImageTk.PhotoImage(imagem_resized)
+
+l_logo_imagem = Label(frame_logo, height=60, image=imagem_tk, compound=LEFT, padx=10, anchor='nw', font=('Ivy 16 bold'), bg=co1, fg=co3)
+l_logo_imagem.place(x=18, y=0)
+l_logo_nome = Label(frame_logo,text='Internet Speed Test', height=60, padx=10, anchor=NE, font=('Ivy 20'), bg=co1, fg=co4)
+l_logo_nome.place(x=75, y=10)
+
+l_logo_linha = Label(frame_logo,width=350, anchor=NW, font=('Ivy 1'), bg=co2)
+l_logo_linha.place(x=0, y=57)
 
 janela.mainloop()
